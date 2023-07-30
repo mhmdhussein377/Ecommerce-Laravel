e3c42e43017536f06d3507e66b9a1b778b13a4fe
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-07-30 21:51:30 +0300

    implemented the delete category feature

--- Client/scripts/categoriesDashboard.js
+++ Client/scripts/categoriesDashboard.js
@@ -4,7 +4,12 @@ const closeModalButton = document.querySelector(".modal .close-modal");
 const categoryNameCreate = document.getElementById("create-category-input")
 const creaeteCategoryButton = document.getElementById('create-category-button')
 const createCategoryBox = document.querySelector(".create-category-box")
+const deleteSelect = document.getElementById("delete-select")
+const deleteCategoryForm = document.querySelector(".delete-category-box");
+const acceptDeleteButton = document.getElementById("accept-delete-button");
 const categoryCreated = document.querySelector(".category-created")
+const categoryDeleted = document.querySelector(".category-deleted");
+
 
 deleteButton.addEventListener("click", () => {
     confirmDeleteModal
@@ -18,6 +23,7 @@ closeModalButton.addEventListener("click", () => {
         .add("hide");
 });
 
+// craete new category
 createCategoryBox.addEventListener('submit', async(e) => {
     e.preventDefault()
 
@@ -25,19 +31,53 @@ createCategoryBox.addEventListener('submit', async(e) => {
     let token = localStorage.getItem("token")
 
     try {
-        let response = await axios.post("http://127.0.0.1:8000/api/create_category", {category_name: categoryName}, {
+        let response = await axios.post("http://127.0.0.1:8000/api/create_category", {
+            category_name: categoryName
+        }, {
             headers: {
                 'Authorization': `Bearer ${token}`
             }
         });
 
         categoryNameCreate.value = ""
-        categoryCreated.classList.remove("hide")
+        categoryCreated
+            .classList
+            .remove("hide")
         setTimeout(() => {
-            categoryCreated.classList.add("hide")
+            categoryCreated
+                .classList
+                .add("hide")
         }, 2000)
 
     } catch (error) {
         console.log(error)
     }
+})
+
+// delete a category
+deleteCategoryForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+})
+
+acceptDeleteButton.addEventListener("click", async() => {
+
+    const category = deleteSelect.value;
+    const token = localStorage.getItem("token")
+
+    try {
+        const response = await axios.delete("http://127.0.0.1:8000/api/delete_category/1", {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        confirmDeleteModal.classList.add("hide")
+        categoryDeleted.classList.remove("hide")
+        setTimeout(() => {
+            categoryDeleted.classList.add("hide")
+        }, 2000)
+    } catch (error) {
+        console.log(error)
+    }
+
+    console.log(category)
 })
