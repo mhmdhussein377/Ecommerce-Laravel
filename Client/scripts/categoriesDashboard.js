b521b26d2f5031e0b5d1e31d52decdae82e6454c
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-07-30 20:05:49 +0300

    implemented the create new category functionality

--- Client/scripts/categoriesDashboard.js
+++ Client/scripts/categoriesDashboard.js
@@ -1,11 +1,43 @@
 const deleteButton = document.querySelector(".delete-button");
 const confirmDeleteModal = document.querySelector(".modal");
 const closeModalButton = document.querySelector(".modal .close-modal");
+const categoryNameCreate = document.getElementById("create-category-input")
+const creaeteCategoryButton = document.getElementById('create-category-button')
+const createCategoryBox = document.querySelector(".create-category-box")
+const categoryCreated = document.querySelector(".category-created")
 
 deleteButton.addEventListener("click", () => {
-    confirmDeleteModal.classList.remove("hide");
+    confirmDeleteModal
+        .classList
+        .remove("hide");
 });
 
 closeModalButton.addEventListener("click", () => {
-    confirmDeleteModal.classList.add("hide");
+    confirmDeleteModal
+        .classList
+        .add("hide");
 });
+
+createCategoryBox.addEventListener('submit', async(e) => {
+    e.preventDefault()
+
+    let categoryName = categoryNameCreate.value
+    let token = localStorage.getItem("token")
+
+    try {
+        let response = await axios.post("http://127.0.0.1:8000/api/create_category", {category_name: categoryName}, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+
+        categoryNameCreate.value = ""
+        categoryCreated.classList.remove("hide")
+        setTimeout(() => {
+            categoryCreated.classList.add("hide")
+        }, 2000)
+
+    } catch (error) {
+        console.log(error)
+    }
+})
---
+++ Client/scripts/create-product.js
@@ -0,0 +1,70 @@
+const form = document.querySelector("form")
+const productImg = document.getElementById("product-img")
+const productNameInput = document.getElementById("product-name-input")
+const productDescInput = document.getElementById("product-desc-input")
+const categories = document.getElementById("categories")
+const uploadImageButton = document.getElementById("upload-img")
+const createButton = document.querySelector(".create-button button")
+const fileInput = document.getElementById("file-input")
+
+uploadImageButton.addEventListener("click", () => {
+    fileInput.click()
+})
+
+let base64 = "";
+
+fileInput.addEventListener("input", (e) => {
+    if (e.target.files.length > 0) {
+        function getBase64(file) {
+            return new Promise((resolve, reject) => {
+                const reader = new FileReader();
+                reader.readAsDataURL(file);
+                reader.onload = () => resolve(reader.result);
+                reader.onerror = (error) => reject(error);
+            });
+        }
+        getBase64(e.target.files[0]).then((data) => {
+            productImg.style.display = "block";
+            productImg.src = data;
+        });
+
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            base64 = reader.result;
+        };
+        reader.readAsDataURL(e.target.files[0]);
+    }
+});
+
+form.addEventListener('submit', async(e) => {
+    e.preventDefault()
+
+    let token = localStorage.getItem("token")
+    let productName = productNameInput.value
+    let productDesc = productDescInput.value
+    let category = categories.value
+
+    console.log(token)
+
+    try {
+        let data = {
+            name: productName,
+            description: productDesc,
+            image: base64,
+            category_id: category
+        }
+
+        const headers = {
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json"
+        };
+
+        let response = await axios.post("http://127.0.0.1:8000/api/create_product", data, {
+            headers: headers
+        });
+        console.log("response")
+        console.log(response)
+    } catch (error) {
+        console.log(error)
+    }
+})
