d86e81e33aa114c9508e43406d023f1afcce9216
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-07-31 01:43:54 +0300

    added some responsiveness

--- Client/scripts/favorites.js
+++ Client/scripts/favorites.js
@@ -14,7 +14,6 @@ try {
             }
         });
 
-        let products = []
         async function getProduct(productID) {
             try {
                 const response = await axios.get(`http://127.0.0.1:8000/api/products/${productID}`, {
@@ -44,7 +43,6 @@ try {
                 });
         }
         fillProductsArray()
-        console.log(products)
     }
     getFavorites();
 } catch (error) {
