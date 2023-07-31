49f6f6136658dc81038a8009bed1f895bb0e85a3
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-07-31 14:14:18 +0300

    implemented the add to cart functionality

--- Client/scripts/home.js
+++ Client/scripts/home.js
@@ -33,7 +33,7 @@ logout.addEventListener("click", async() => {
     }
 })
 
-// displaying items
+// add to favorites / remove from favorites
 async function addToFavorites(event, product_id) {
     const user_id = JSON
         .parse(localStorage.getItem("user"))
@@ -58,6 +58,33 @@ async function addToFavorites(event, product_id) {
         console.log(response);
     } catch (error) {
         console.log(error)
+    }
+}
+
+// add to cart / remove from cart functionality
+async function addToCart(event, product_id) {
+    const user_id = JSON
+        .parse(localStorage.getItem("user"))
+        .id;
+    const token = localStorage.getItem("token");
+    const data = {
+        user_id,
+        product_id
+    };
+
+    try {
+        let response = await axios.post("http://127.0.0.1:8000/api/cart/add", data, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        if (response.data.message == "Item added to cart successfully") {
+            event.target.innerText = "Remove from Cart";
+        } else {
+            event.target.innerText = "Add to Cart";
+        }
+    } catch (error) {
+        console.log(error);
     }
 }
 
@@ -91,22 +118,37 @@ try {
                                         .id,
                                     product_id: product.id
                                 };
-                                const response = await axios.post("http://127.0.0.1:8000/api/is_favorited", data);
-                                return response.data === "Yes";
+                                const isFavoritedResponse = await axios.post("http://127.0.0.1:8000/api/is_favorited", data);
+                                const isInCartResponse = await axios.post("http://127.0.0.1:8000/api/is_item_in_cart", data);
+                                console.log("-----------")
+                                console.log(product.id)
+                                console.log(isFavoritedResponse, isInCartResponse)
+                                console.log(product.id)
+                                console.log("-----------")
+                                let isFavoritedAndInCart = [
+                                    isFavoritedResponse.data === "Yes"
+                                        ? true
+                                        : false,
+                                    isInCartResponse.data === "Yes"
+                                        ? true
+                                        : false
+                                ];
+                                return isFavoritedAndInCart
                             } catch (error) {
                                 console.log(error);
                             }
                         });
                     }
-                    const favorites = checkFavoritesForProducts(data.products)
+                    const favoritesAndInCart = checkFavoritesForProducts(data.products)
                     Promise
-                        .all(favorites)
+                        .all(favoritesAndInCart)
                         .then((favoritesArray) => {
                             categoriesElement.innerHTML += `<div class="category">
                     <h3>${data.category_name}</h3>
                     <div class="cards">
                         ${data.products
                                 ?.map(function (product, index) {
+                                    console.log(favoritesArray[index])
                                     return `<div class="card"> <div class="card-img">
                                             <img src="./../assets/product_01.jpg" alt=""></div>
                                             <div class="card-name">${
@@ -118,41 +160,31 @@ try {
                                                     <button
                                                         class="add-favorite-button"
                                                         onClick="addToFavorites(event, ${
-                                    product.id})">${favoritesArray[index]
+                                    product.id})">${favoritesArray[index][0]
                                         ? "Remove from Favorites"
                                         : "Add to Favorites"}</button>
                                                 </div>
                                                 <div>
-                                                    <button>Add to Cart</button>
+                                                    <button onClick="addToCart(event, ${
+                                    product.id})">${favoritesArray[index][1]
+                                        ? "Remove from Cart"
+                                        : "Add to Cart"}</button>
                                                 </div>
                                             </div>
                                         </div>`;})}
                     </div>
                 </div>`;
-                                })
+                                });
                         }})
-                // let organizedData = {} response.data.forEach(product => {     const
-                // categoryId = product.category_id;     if(!organizedData[categoryId]) {
-                // organizedData[categoryId] = []     } organizedData[categoryId].push(product)
-                // }) console.log(organizedData)
             }
             getProduts()
         } catch (error) {
             console.log(error)
         }
 
-        let isFavorited = false;
-    async function checkFavorite() {
-        try {
-            const data = {
-                user_id: JSON
-                    .parse(localStorage.getItem("user"))
-                    .id,
-                product_id: product.id
-            };
-            const response = await axios.post("http://127.0.0.1:8000/api/is_favorited", data);
-            console.log(response.data);
-        } catch (error) {
-            console.log(error);
-        }
-    }
+        //     let isFavorited = false; async function checkFavorite() {     try { const
+        // data = {             user_id: JSON .parse(localStorage.getItem("user")) .id,
+        // product_id: product.id         };         const response = await
+        // axios.post("http://127.0.0.1:8000/api/is_favorited", data);
+        // console.log(response.data);     } catch (error) {         console.log(error);
+        //     } }
